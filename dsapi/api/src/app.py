284acd1d8936cbda1504bb221533ccf18f463db7
jdnascim <jose.dori.nascimento@gmail.com>
2019-10-04 15:43:00 -0300

    added conf_json path

--- dsapi/api/src/app.py
+++ dsapi/api/src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_json import FlaskJSON, as_json
 from core.base_api import base_blueprint
 import json as j
+import os
 
 from social_tracker_library import collection as col
 
@@ -9,10 +10,12 @@ app: Flask = Flask(__name__)
 app.register_blueprint(base_blueprint)
 json: FlaskJSON = FlaskJSON(app)
 
+conf_json = os.path.dirname(os.path.abspath(__file__)) + "/conf.json"
+
 
 @app.route("/ds-api/collections/list_collections", methods=['GET'])
 def list_collections():
-    col.conf_json = "./conf.json"
+    col.conf_json = conf_json
     collections = col.list_collections(print_res=False)
 
     if collections is None:
