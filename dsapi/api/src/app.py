daf942deb18646b082b3d54c527000df74dec712
jdnascim <jose.dori.nascimento@gmail.com>
2019-10-10 16:06:34 -0300

    new call - extract

--- dsapi/api/src/app.py
+++ dsapi/api/src/app.py
@@ -18,46 +18,31 @@ col.conf_json = conf_json
 def list_collections():
     collections = col.list_collections(print_res=False)
 
-    if collections is None:
+    try:
+        if collections is None:
+            return app.response_class(status=404, mimetype='application/json')
+
+        return app.response_class(
+            response=j.dumps([col for col in collections]),
+            status=200,
+            mimetype='application/json'
+        )
+    except Exception:
         return app.response_class(status=404, mimetype='application/json')
 
-    return app.response_class(
-        response=j.dumps([col for col in collections]),
-        status=200,
-        mimetype='application/json'
-    )
 
-
-@app.route("/dsapi/collections/create", methods=['POST'])
-@as_json
-def create_collection():
+@app.route("/dsapi/collection/extract_collection", methods=['POST'])
+def extract_collection():
     try:
-        content = request.get_json()
+        title = request.args.get("title")
+        ownerId = request.args.get("ownerId")
+        start_date = request.args.get("start_date")
+        end_date = request.args.get("end_date")
+        original = request.args.get("original")
 
-        title = content['title']
-        ownerId = content['ownerId']
-        keywords = list(content['keywords'])
-        start_date = content['start_date']
-
-        col(title, ownerId, start_date).create(keywords)
+        col(title, ownerId, start_date, end_date,
+            original).extract_collection()
 
         return app.response_class(status=200, mimetype='application/json')
     except Exception:
-        return app.response_class(status=404)
-
-
-@app.route("/dsapi/collection/itemCount")
-@as_json
-def item_count():
-    try:
-        content = request.get_json()
-
-        title = content['title']
-        ownerId = content['ownerId']
-        start_date = content['start_date']
-        end_date = content['end_date']
-        original = content['original']
-
-        col(title, ownerId, start_date, end_date, original).item_count()
-    except Exception:
-        return app.response_class(status=404)
+        return app.response_class(status=404, mimetype='application/json')
