923efe6c71944dbc6064772a83564e2ff546c668
jdnascim <jose.dori.nascimento@gmail.com>
2019-10-10 13:08:16 -0300

    new calls

--- dsapi/api/src/app.py
+++ dsapi/api/src/app.py
@@ -11,11 +11,11 @@ app.register_blueprint(base_blueprint)
 json: FlaskJSON = FlaskJSON(app)
 
 conf_json = os.path.dirname(os.path.abspath(__file__)) + "/conf.json"
+col.conf_json = conf_json
 
 
-@app.route("/ds-api/collections/list_collections", methods=['GET'])
+@app.route("/dsapi/collections/list_collections", methods=['GET'])
 def list_collections():
-    col.conf_json = conf_json
     collections = col.list_collections(print_res=False)
 
     if collections is None:
@@ -26,3 +26,38 @@ def list_collections():
         status=200,
         mimetype='application/json'
     )
+
+
+@app.route("/dsapi/collections/create", methods=['POST'])
+@as_json
+def create_collection():
+    try:
+        content = request.get_json()
+
+        title = content['title']
+        ownerId = content['ownerId']
+        keywords = list(content['keywords'])
+        start_date = content['start_date']
+
+        col(title, ownerId, start_date).create(keywords)
+
+        return app.response_class(status=200, mimetype='application/json')
+    except Exception:
+        return app.response_class(status=404)
+
+
+@app.route("/dsapi/collection/itemCount")
+@as_json
+def item_count():
+    try:
+        content = request.get_json()
+
+        title = content['title']
+        ownerId = content['ownerId']
+        start_date = content['start_date']
+        end_date = content['end_date']
+        original = content['original']
+
+        col(title, ownerId, start_date, end_date, original).item_count()
+    except Exception:
+        return app.response_class(status=404)
