bd16f8c3d8b3082a316346032299069255d6930f
jdnascim <jose.dori.nascimento@gmail.com>
2019-10-13 18:55:21 -0300

    add and remove keywords functions

--- dsapi/api/src/app.py
+++ dsapi/api/src/app.py
@@ -34,6 +34,46 @@ def list_collections():
         return app.response_class(status=404, mimetype='application/json')
 
 
+@app.route("/dsapi/collections/add_keywords", methods=['PUT'])
+def add_keywords():
+    title = request.args.get("title")
+    ownerId = request.args.get("ownerId")
+    keywords = request.args.get("keywords")
+
+    keywords = keywords.split("//")
+
+    try:
+        col(title, ownerId).add_keywords(keywords)
+
+        return app.response_class(
+            status=200,
+            mimetype='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return app.response_class(status=404, mimetype='application/json')
+
+
+@app.route("/dsapi/collections/remove_keywords", methods=['PUT'])
+def remove_keywords():
+    title = request.args.get("title")
+    ownerId = request.args.get("ownerId")
+    keywords = request.args.get("keywords")
+
+    keywords = keywords.split("//")
+
+    try:
+        col(title, ownerId).remove_keywords(keywords)
+
+        return app.response_class(
+            status=200,
+            mimetype='application/json'
+        )
+    except Exception as e:
+        print(e)
+        return app.response_class(status=404, mimetype='application/json')
+
+
 @app.route("/dsapi/extraction/extract_collection", methods=['GET'])
 def extract_collection():
     try:
@@ -43,21 +83,58 @@ def extract_collection():
         end_date = request.args.get("end_date")
         original = request.args.get("original")
 
-        res = col(title, ownerId, start_date, end_date,
-                  original).extract_collection()
+        col(title, ownerId, start_date, end_date,
+            original).extract_collection()
 
-        return app.response_class(response=j.dumps(res), status=200,
+        return app.response_class(response=j.dumps("OK"), status=200,
                                   mimetype='application/json')
     except Exception as e:
         print(e)
         return app.response_class(status=404, mimetype='application/json')
 
 
-@app.route("/dsapi/extraction/scrap_media", methods['GET'])
+@app.route("/dsapi/extraction/scrap_media", methods=['GET'])
 def scrap_media():
     try:
         collection = request.args.get("collection")
-        mode = requests.args.get("mode")
+        mode = request.args.get("mode")
 
-        if mode.lower() == 'i':
-            extr("")
+        if mode.lower() in ('i', 'image', 'v', 'video'):
+            extr(collection).media_csv_download(type_file=mode)
+        elif mode.lower() in ('b', 'both'):
+            extr(collection).media_csv_download(type_file='i')
+            extr(collection).media_csv_download(type_file='v')
+        else:
+            return app.response_class(status=404, mimetype='application/json')
+
+        return app.response_class(response=j.dumps("OK"), status=200,
+                                  mimetype='application/json')
+    except Exception as e:
+        print(e)
+        return app.response_class(status=404, mimetype='application/json')
+
+
+@app.route("/dsapi/extraction/expand_text", methods=['GET'])
+def expand_text():
+    try:
+        collection = request.args.get("collection")
+
+        extr(collection).expand_text()
+
+        return app.response_class(response=j.dumps("OK"), status=200,
+                                  mimetype='application/json')
+    except Exception as e:
+        print(e)
+        return app.response_class(status=404, mimetype='application/json')
+
+
+@app.route("/dsapi/extraction/list_extracted_collections", methods=['GET'])
+def list_extracted_collections():
+    try:
+        cols = extr.list_extracted_collections()
+
+        return app.response_class(response=j.dump(cols), status=200,
+                                  mimetype='application/json')
+    except Exception as e:
+        print(e)
+        return app.response_class(status=404, mimetype='application/json')
