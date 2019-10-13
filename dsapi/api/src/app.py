b16225cd5b0ba8c137db4d85009bd7929a4822f4
jdnascim <jose.dori.nascimento@gmail.com>
2019-10-12 23:30:21 -0300

    conf_json to extractor class

--- dsapi/api/src/app.py
+++ dsapi/api/src/app.py
@@ -5,6 +5,7 @@ import json as j
 import os
 
 from social_tracker_library import collection as col
+from social_tracker_library import extractor as extr
 
 app: Flask = Flask(__name__)
 app.register_blueprint(base_blueprint)
@@ -12,6 +13,7 @@ json: FlaskJSON = FlaskJSON(app)
 
 conf_json = os.path.dirname(os.path.abspath(__file__)) + "/conf.json"
 col.conf_json = conf_json
+extr.conf_json = conf_json
 
 
 @app.route("/dsapi/collections/list_collections", methods=['GET'])
@@ -32,7 +34,7 @@ def list_collections():
         return app.response_class(status=404, mimetype='application/json')
 
 
-@app.route("/dsapi/collections/extract_collection", methods=['POST'])
+@app.route("/dsapi/extraction/extract_collection", methods=['GET'])
 def extract_collection():
     try:
         title = request.args.get("title")
@@ -41,10 +43,21 @@ def extract_collection():
         end_date = request.args.get("end_date")
         original = request.args.get("original")
 
-        col(title, ownerId, start_date, end_date,
-            original).extract_collection()
+        res = col(title, ownerId, start_date, end_date,
+                  original).extract_collection()
 
-        return app.response_class(status=200, mimetype='application/json')
+        return app.response_class(response=j.dumps(res), status=200,
+                                  mimetype='application/json')
     except Exception as e:
         print(e)
         return app.response_class(status=404, mimetype='application/json')
+
+
+@app.route("/dsapi/extraction/scrap_media", methods['GET'])
+def scrap_media():
+    try:
+        collection = request.args.get("collection")
+        mode = requests.args.get("mode")
+
+        if mode.lower() == 'i':
+            extr("")
